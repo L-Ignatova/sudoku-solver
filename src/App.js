7597c71dab4a87f626604df52dc972e48051ad3c
Liliya Ignatova <liliya.oignatova@gmail.com>
2022-01-23 10:01:52 +0200

    grid html and css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import {solve} from "./solutions/solutionOne";
-
+import Grid from "./Grid";
 
 function App() {
   return (
     <div className="App">
+      <Grid />
       <button onClick={solve}>Solve</button>
       {/* {solve()} */}
     </div>
---
+++ src/Grid.js
@@ -0,0 +1,109 @@
+import React from "react";
+
+const Grid = () => {
+  return (
+    <div className="table">
+      <div className="row">
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+      </div>
+      <div className="row">
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+      </div>
+      <div className="row">
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+      </div>
+      <div className="row">
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+      </div>
+      <div className="row">
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+      </div>
+      <div className="row">
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+      </div>
+      <div className="row">
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+      </div>
+      <div className="row">
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+      </div>
+      <div className="row">
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+        <div className="cell"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Grid;
