0562e3d715d63f15d32e651e81a7d771e378fd02
Liliya Ignatova <liliya.oignatova@gmail.com>
2022-01-23 09:37:13 +0200

    update solutionOne solve() loops

--- src/solutions/solutionOne.js
+++ src/solutions/solutionOne.js
@@ -32,46 +32,46 @@ const notInBox = (el, row, col, g) => {
   return !box.includes(el);
 };
 
-let grid = [
-  [0, 0, 0, 0, 0, 0, 4, 0, 3],
-  [0, 0, 0, 3, 0, 0, 5, 0, 9],
-  [0, 3, 0, 5, 6, 9, 7, 1, 0],
-  [0, 6, 7, 0, 0, 0, 0, 4, 0],
-  [0, 4, 0, 0, 3, 0, 2, 9, 0],
-  [0, 2, 0, 0, 0, 0, 6, 8, 0],
-  [0, 0, 2, 0, 0, 1, 9, 0, 4],
-  [0, 0, 3, 8, 0, 5, 0, 7, 6],
-  [0, 7, 0, 4, 9, 0, 8, 5, 2]
-];
-
 // let grid = [
-//   [0, 0, 9, 0, 0, 0, 4, 6, 3],
-//   [0, 0, 6, 3, 4, 0, 5, 2, 9],
-//   [2, 3, 4, 5, 6, 9, 7, 1, 8],
-//   [0, 6, 7, 0, 0, 0, 3, 4, 0],
+//   [0, 0, 0, 0, 0, 0, 4, 0, 3],
+//   [0, 0, 0, 3, 0, 0, 5, 0, 9],
+//   [0, 3, 0, 5, 6, 9, 7, 1, 0],
+//   [0, 6, 7, 0, 0, 0, 0, 4, 0],
 //   [0, 4, 0, 0, 3, 0, 2, 9, 0],
 //   [0, 2, 0, 0, 0, 0, 6, 8, 0],
-//   [0, 0, 2, 0, 0, 1, 9, 3, 4],
-//   [4, 9, 3, 8, 2, 5, 1, 7, 6],
-//   [0, 7, 0, 4, 9, 3, 8, 5, 2]
+//   [0, 0, 2, 0, 0, 1, 9, 0, 4],
+//   [0, 0, 3, 8, 0, 5, 0, 7, 6],
+//   [0, 7, 0, 4, 9, 0, 8, 5, 2]
 // ];
+
+let grid = [
+  [0, 0, 9, 0, 0, 0, 4, 6, 3],
+  [0, 0, 6, 3, 4, 0, 5, 2, 9],
+  [2, 3, 4, 5, 6, 9, 7, 1, 8],
+  [0, 6, 7, 0, 0, 0, 3, 4, 0],
+  [0, 4, 0, 0, 3, 0, 2, 9, 0],
+  [0, 2, 0, 0, 0, 0, 6, 8, 0],
+  [0, 0, 2, 0, 0, 1, 9, 3, 4],
+  [4, 9, 3, 8, 2, 5, 1, 7, 6],
+  [0, 7, 0, 4, 9, 3, 8, 5, 2]
+];
 
 export const solve = () => {
   const range = Array.from(Array(9).keys(), x => x + 1);
   let unsolved = isZeroInGrid(grid);
   let round = 0;
-  
+
   while (unsolved) {
     let newGrid = [];
     let possibleValues = {};
     round += 1;
 
-    for (const [i, row] of grid.entries()) {
+    grid.forEach((row, i) => {
       newGrid.push([]);
 
-      for (const [j, val] of row.entries()) {
+      row.forEach((val, j) => {
         if (val === 0) {
-          let identifier = `(${i}, ${j})`;
+          const identifier = `(${i}, ${j})`;
 
           possibleValues[identifier] = range.filter(val => {
             return (
@@ -85,8 +85,8 @@ export const solve = () => {
         } else {
           newGrid[i].push(val);
         }
-      }
-    }
+      });
+    });
     // debugger
     let match = gridsMatch(grid, newGrid);
 
