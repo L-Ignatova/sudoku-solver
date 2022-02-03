10c2aba8e6c77e3d2b73444d69135aa154d1daca
Liliya Ignatova <liliya.oignatova@gmail.com>
2022-02-03 15:00:48 +0200

    solution two - backtracking algorithm

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import {solve} from "./solutions/solutionOne";
+import {solve, grid} from "./solutions/solutionTwo";
 import Grid from "./Grid";
 
 function App() {
   return (
     <div className="App">
       <Grid />
-      <button onClick={solve}>Solve</button>
+      <button onClick={() => solve(grid)}>Solve</button>
     </div>
   );
 }
--- src/solutions/solutionTwo.js
+++ src/solutions/solutionTwo.js
@@ -1,3 +1,77 @@
+import {
+  notInRowOrCol,
+  notInBox,
+  isZeroInGrid,
+  printGrid,
+} from "./utils";
 
+export let grid = [
+  [0, 0, 0, 8, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 4, 3],
+  [5, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 7, 0, 8, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 0, 0],
+  [0, 2, 0, 0, 3, 0, 0, 0, 0],
+  [6, 0, 0, 0, 0, 0, 0, 7, 5],
+  [0, 0, 3, 4, 0, 0, 0, 0, 0],
+  [0, 0, 0, 2, 0, 0, 6, 0, 0]
+];
 
+// export let grid = [
+//   [0, 0, 9, 0, 0, 0, 4, 6, 3],
+//   [0, 0, 6, 3, 4, 0, 5, 2, 9],
+//   [2, 3, 4, 5, 6, 9, 7, 1, 8],
+//   [0, 6, 7, 0, 0, 0, 3, 4, 0],
+//   [0, 4, 0, 0, 3, 0, 2, 9, 0],
+//   [0, 2, 0, 0, 0, 0, 6, 8, 0],
+//   [0, 0, 2, 0, 0, 1, 9, 3, 4],
+//   [4, 9, 3, 8, 2, 5, 1, 7, 6],
+//   [0, 7, 0, 4, 9, 3, 8, 5, 2]
+// ];
 
+const range = Array.from(Array(9).keys(), x => x + 1);
+let round = 0;
+
+export const solve = (grid) => {
+  let newGrid = [];
+  
+  if (!isZeroInGrid(grid)) {
+    printGrid(grid, round);
+    return true;
+  }
+  round += 1;
+
+  for (let [i, row] of grid.entries()) {
+    newGrid.push([]);
+    
+    for (let [j, val] of row.entries()) {
+      if (val === 0) {
+        for (let possibleValue of range) {
+          if (notInBox(possibleValue, i, j, grid) && notInRowOrCol(possibleValue, i, j, grid)) {
+            let gridWithPossibleVal = newGrid.map(g => g.slice());
+            gridWithPossibleVal[i].push(possibleValue);
+
+            if (j < row.length - 1) {
+              for (let m = j + 1; m < row.length; m++) {
+                gridWithPossibleVal[i].push(grid[i][m]);
+              }
+            }
+
+            if (i < grid.length - 1) {
+              for (let n = i + 1; n < grid.length; n++) {
+                gridWithPossibleVal.push(grid[n]);
+              }
+            }
+            if (solve(gridWithPossibleVal)) {
+              return true;
+            } 
+          }
+        }
+        return false;
+      } else {
+        newGrid[i].push(val);
+      }
+    }
+  }
+
+}
